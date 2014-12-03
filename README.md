38900eb99d539457ea5e8c19e1068a291790a457
Mattias Hoelzl <tc@xantira.com>
2014-12-03 07:59:28 +0100

    Improve handling of atomic tasks

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# ExtendedBehaviorTrees
+# Extended Behavior Trees
+
+This is an implementation of Extended Behavior Trees (XBTs) in Julia.
+
+See `src/ExtendedBehaviorTrees.pdf` or
+`src/ExtendedBehaviorTrees.html` for more information.
+
+The file `src/ExtendedBehaviorTrees.org` contains the complete source
+code and documentation.  The files `src/ExtendedBehaviorTrees.*` are
+generated from this file and contain the formated documentation in
+HTML and PDF format as well as the extracted Julia source code.
 
 [![Build Status](https://travis-ci.org/hoelzl/ExtendedBehaviorTrees.jl.svg?branch=master)](https://travis-ci.org/hoelzl/ExtendedBehaviorTrees.jl)
--- src/ExtendedBehaviorTrees.jl
+++ src/ExtendedBehaviorTrees.jl
@@ -7,43 +7,120 @@
 
 module ExtendedBehaviorTrees
 
+export XbtNodeStatus, Inactive, Running, Succeeded, Failed;
+
 abstract XbtNodeStatus;
+immutable Inactive <: XbtNodeStatus end;
+immutable Running <: XbtNodeStatus end;
 immutable Succeeded <: XbtNodeStatus
     value
 end;
 immutable Failed <: XbtNodeStatus end;
-immutable Running <: XbtNodeStatus end;
-typealias XbtResult (XbtNodeStatus,Bool);
 
-type Env
-end;
+export isinactive, isrunning, issucceeded, isfailed;
 
+isinactive(x) = false;
+isinactive(x::Inactive) = true;
+
+isrunning(x) = false;
+isrunning(x::Running) = true;
+
+issucceeded(x) = false;
+issucceeded(x::Succeeded) = true;
+
+isfailed(x) = false;
+isfailed(x::Failed) = true;
+
+export XbtNodeResult;
+typealias XbtNodeResult (XbtNodeStatus, Bool);
+
+export isdone;
+isdone(x) = false;
+isdone(x::XbtNodeResult) = isdone(x...);
+isdone(x, cont) = false;
+isdone(x::Succeeded, cont::Bool) = !cont;
+isdone(x::Failed, cont::Bool) = true;
+
+export failed, running, succeeded;
+const failed = (Failed(), false);
+const running = (Running(), true);
+succeeded(val, cont=false) = (Succeeded(val), cont);
+
+export XbtNode, AtomicXbtNode, CompositeXbtNode;
 abstract XbtNode;
 abstract AtomicXbtNode <: XbtNode;
 abstract CompositeXbtNode <: XbtNode;
 
-immutable XbtTask <: AtomicXbtNode
+export status, setstatus;
+status(node::XbtNode) = node.status;
+setstatus(node::XbtNode, status::XbtNodeStatus) = node.status = status;
+
+export cont, setcont;
+cont(node::XbtNode) = node.cont;
+setcont(node::XbtNode, cont::Bool) = node.cont = cont;
+
+export result, setresult;
+result(node::XbtNode) = status(node), cont(node);
+
+function setresult(node::XbtNode, result::XbtNodeResult)
+    setstatus(node, result[1])
+    setcont(node, result[2])
+    result
+end;
+
+isinactive(node::XbtNode) = isinactive(status(node));
+isrunning(node::XbtNode) = isrunning(status(node));
+issucceeded(node::XbtNode) = issucceeded(status(node));
+isfailed(node::XbtNode) = isfailed(status(node));
+isdone(node::XbtNode) = isdone(status(node), cont(node));
+
+export setinactive;
+function setinactive(node::XbtNode)
+    setstatus(node, Inactive());
+    setcont(node, true); # Slightly superfluous
+end;
+
+type XbtTask <: AtomicXbtNode
     task::Task
+    status::XbtNodeStatus
+    cont::Bool
 end;
 
-function tick(node::XbtTask, env)
-    consume(node.task);
+function XbtTask(fun::Function, status::XbtNodeStatus, cont::Bool)
+    XbtTask(Task(fun), status, cont);
+end;
+XbtTask(task, status) = XbtTask(task, status, false);
+XbtTask(task) = XbtTask(task, Inactive());
+
+function tick(node::XbtTask, state)
+    if (isdone(node))
+        return result(node)
+    end
+    setresult(node, consume(node.task))
 end;
 
-immutable XbtFun <: AtomicXbtNode
+type XbtFun <: AtomicXbtNode
     fun::Function
+    status::XbtNodeStatus
+    cont::Bool
 end;
 
-function tick(node::XbtFun, Env)
-    node.fun();
+XbtFun(fun::Function, status::XbtNodeStatus) = XbtFun(task, status, false);
+XbtFun(fun::Function) = XbtFun(fun, Inactive());
+
+function tick(node::XbtFun, state)
+    if (isdone(node))
+        return result(node)
+    end
+    setresult(node, node.fun())
 end;
 
 abstract XbtSequenceNode <: CompositeXbtNode;
 
-function tick(node::XbtSequenceNode, env::Env)
+function tick(node::XbtSequenceNode, state)
     local sum = 0, status, cont;
     for child in node.children
-        status, cont = tick(child);
+        status, cont = tick(child, state);
         if isa(status, Failed) return (status, false) end;
         if isa(status, Running) return (status, true) end;
         sum += status.value;
@@ -58,10 +135,10 @@ end;
 
 abstract XbtChoiceNode <: CompositeXbtNode;
 
-function tick(node::XbtChoiceNode, env::Env)
+function tick(node::XbtChoiceNode, state)
     local status, cont;
     for child in node.children
-        status, cont = tick(child);
+        status, cont = tick(child, state);
         if isa(status, Succeeded) return (status, cont) end;
         if isa(status, Running) return (status, true) end;
     end;
